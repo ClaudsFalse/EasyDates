c05e44e24cdc561e2509babea8952ef22254a40d
ClaudsFalse <falsetticlaudia@gmail.com>
2022-08-04 15:08:37 +0100

    Initial Commit

---
+++ EasyDates/EasyDatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyDatesApp.swift
+//  EasyDates
+//
+//  Created by Claudia Falsetti on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct EasyDatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
